ff052dc1030c051e71e006136c4d1c91500a6106
joshuasze <joshua.sze@pydrox.com>
2026-05-26 00:44:18 +0800

    Revert "fix?"
    This reverts commit b1738833218490620e0738fbe27e8b44359553e2.

--- src/data/index.js
+++ src/data/index.js
@@ -22,15 +22,15 @@ const slides = [
   { id: 1, photo: getSlidePhoto(1), presenterView: P1, audienceView: A1 },
   { id: 2, photo: getSlidePhoto(2), presenterView: P2, audienceView: A2 },
   { id: 3, photo: getSlidePhoto(3), presenterView: P3, audienceView: A3 },
+  { id: 10, photo: getSlidePhoto(10), presenterView: P10, audienceView: A10 },
+  { id: 11, photo: getSlidePhoto(11), presenterView: P11, audienceView: A11 },
+  { id: 12, photo: getSlidePhoto(12), presenterView: P12, audienceView: A12 },
   { id: 4, photo: getSlidePhoto(4), presenterView: P4, audienceView: A4 },
   { id: 5, photo: getSlidePhoto(5), presenterView: P5, audienceView: A5 },
   { id: 6, photo: getSlidePhoto(6), presenterView: P6, audienceView: A6 },
   { id: 7, photo: getSlidePhoto(7), presenterView: P7, audienceView: A7 },
   { id: 8, photo: getSlidePhoto(8), presenterView: P8, audienceView: A8 },
   { id: 9, photo: getSlidePhoto(9), presenterView: P9, audienceView: A9 },
-  { id: 10, photo: getSlidePhoto(10), presenterView: P10, audienceView: A10 },
-  { id: 11, photo: getSlidePhoto(11), presenterView: P11, audienceView: A11 },
-  { id: 12, photo: getSlidePhoto(12), presenterView: P12, audienceView: A12 },
 ];
 
 export default slides;
