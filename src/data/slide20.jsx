b4243a97470a7efa9cf64dc398e4747212e5a7b1
joshuasze <joshua.sze@pydrox.com>
2026-05-26 09:01:51 +0800

    added puns

--- src/data/slide20.jsx
+++ src/data/slide20.jsx
@@ -4,7 +4,7 @@ import { slideText } from "./slideTheme.js";
 function ThankYouContent() {
   return (
     <>
-      <h1 style={slideText.title}>Thank you very much for your time!</h1>
+      <h1 style={slideText.title}>Thanks for bee-ing here</h1>
       <p style={slideText.subtitle}>Don&apos;t forget to &quot;leaf&quot; us a like!!</p>
     </>
   );
